f1abb9c9b5bed1b0844d84e5aa547df74d693531
aleysia18 <aleysiajijah@gmail.com>
2023-06-05 08:18:27 +0800

    commit pertama: selesai sesi pertemuan 3

---
+++ matakuliah/matakuliah_add.php
@@ -0,0 +1,44 @@
+<?php
+if (isset($_POST['Submit'])) {
+    $kode = $_POST['kode'];
+    $nama = $_POST['nama'];
+    $sks = $_POST['sks'];
+    $semester = $_POST['semester'];
+
+    $result = mysqli_query($con, "INSERT INTO Matakuliah(kode,nama,sks,semester) VALUES('$kode','$nama','$sks','$semester')") or die(mysqli_error($con));
+
+    header("Location:?page=matakuliah-show");
+}
+?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <strong>Tambah Data Matakuliah</strong>
+            </div>
+            <div class="card-body">
+                <form method="POST" action="?page=matakuliah-add" class="form-horizontal">
+                    <div class="form-group">
+                        <label for="kode" class="ontrol-label">Kode</label>
+                        <input type="text" class="form-control" name="kode" placeholder="Masukan Kode..." required>
+                    </div>
+                    <div class="form-group">
+                        <label for="nama" class="control-label">Nama </label>
+                        <input type="text" class="form-control" name="nama" placeholder="Masukan Nama..." required>
+                    </div>
+                    <div class="form-group">
+                        <label for="sks" class="control-label">SKS</label>
+                        <input type="text" class="form-control" name="sks" placeholder="Masukan SKS..." required>
+                    </div>
+                    <div class="form-group">
+                        <label for="semester" class="control-label">semester</label>
+                        <input type="text" class="form-control" name="semester" placeholder="Masukan Semester..." required>
+                    </div>
+            </div>
+            <input type="submit" name="Submit" class="btn btn-primary" value="Simpan">
+            <input type="reset" name="reset" class="btn btn-warning" value="Reset">
+            </form>
+        </div>
+    </div>
+</div>
+</div>
---
+++ profile.php
@@ -0,0 +1,48 @@
+<?php include "header.php" ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/aley.jpg" alt="Aleysia Ajijah" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIM :</strong></td>
+                        <td align="left" width="50%">2110010165</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Aleysia Ajijah</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 18 Agustus 2003</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0812 5878 0077</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">aleysiajijah@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://instagram.com/lysj2h_?igshid=MzNlNGNkZWQ4Mg==" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="https://twitter.com/Shiyun_18?t=XhRKrlfgCBtdUckksCd2Fg&s=09" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/twitter.svg" width="30" alt="twitter">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
